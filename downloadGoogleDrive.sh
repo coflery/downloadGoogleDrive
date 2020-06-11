a49188860b2ce211f7efc4cd11025969ad36dec9
TinderSmith <coflery@gmail.com>
2020-06-11 19:50:22 +0800

    first commit
    1.chmod 755 for the script
    2.usage: ./downloadGoogleDrive.sh googleFileID fileName
    3.wait until file download finish

---
+++ downloadGoogleDrive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -ne 2 ];
+then
+        echo "id and filename should input"
+        exit 1
+fi
+fileid="$1"
+filename="$2"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+rm ./cookie
